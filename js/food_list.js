bec85d3f5478372d943821b613a7d0cfd28575cb
Pgworld15 <sarojsanmaurya07@gmail.com>
2024-06-06 05:38:29 -0700

    Changes in food-list

--- js/food_list.js
+++ js/food_list.js
@@ -1,21 +1,25 @@
-// var name = document.getElementById("name").value;
-// var order = document.getElementById("order").value;
-// var table = document.getElementById("table").value;
-// var special = document.getElementById("special").value;
-// var result = document.getElementById("food-list")
 
-// function foodList(){
+let result = document.getElementById("food-list");
 
-//     result.innerHTML += `
-//         <ul class='list-box'>
-//             <div class='order-name'>
-//                 <li>Food : ${name}</li>
-//                 <li>Order : ${order}</li>
-//                 <li>Quantity : ${table}</li>
-//                 <li>Special-order : ${special}</li>
-//             </div>
-//         </ul>`;
-// }
+
+
+function foodList(){
+    let item = document.getElementById("item").value;
+    let order = document.getElementById("order").value;
+    let table = document.getElementById("table").value;
+    let special = document.getElementById("special").value;
+    
+
+    result.innerHTML += `<ul class='list-box'>
+            <div class='order-name'>
+                <li>Food : ${item}</li>
+                <li>Order : ${order}</li>
+                <li>Quantity : ${table}</li>
+                <li>Special-order : ${special}</li>
+                <li>${title}</li>
+            </div>
+        </ul>`;
+}
 
 // function receiveItem(){
 //     var receive = document.getElementById("custom-order");
@@ -34,22 +38,23 @@
 //         </ul>`;
 // }
 
-var data = window.location.search;
-var param = new URLSearchParams(data);
+// var data = window.location.search;
+// var param = new URLSearchParams(data);
 
-var item = param.get("item");
-var order = param.get("order")
-var table = param.get("table")
-var special = param.get("special")
-var avoid = param.get("avoid")
 
-var orderCollect = [`${item}`,`${order}`,`${table}`,`${special}`]
-document.getElementById("order-list").innerHTML = `<ul>
-    <li>${item}</li>
-    <li>${order}</li>
-    <li>${table}</li>
-    <li>${special}</li>
-    <li>${avoid}</li>
-    <button>Start</button>
-    <li></li>
-</ul>`
+// var item = param.get("item");
+// var order = param.get("order")
+// var table = param.get("table")
+// var special = param.get("special")
+// var avoid = param.get("avoid")
+
+// // var orderCollect = [`${item}`,`${order}`,`${table}`,`${special}`]
+// document.getElementById("order-list").innerHTML += `<ul>
+//     <li>${item}</li>
+//     <li>${order}</li>
+//     <li>${table}</li>
+//     <li>${special}</li>
+//     <li>${avoid}</li>
+//     <button>Start</button>
+//     <li></li>
+// </ul>`
